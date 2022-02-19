d32d854cf9b24d200b19e98ce8a6bee7d9d4f54e
Aleksandr Isakov <isakov.a.n@yandex.ru>
2022-02-19 14:15:24 +0300

    Добавлены Unit тесты класса Cat и Feline

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,41 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    private Cat cat;
+
+    @Mock
+    Feline feline;
+
+    @Before
+    public void init() {
+        cat = new Cat(feline);
+    }
+
+    @Test
+    public void getSound() {
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void getFood() {
+        try {
+            Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+            assertEquals(List.of("Животные", "Птицы", "Рыба"),cat.getFood());
+        } catch (Exception e) {
+            assertNull(e);
+        }
+    }
+}
---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,41 @@
+import com.example.Feline;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FelineTest {
+
+    private Feline feline;
+
+    @Before
+    public void init(){
+        feline = new Feline();
+    }
+
+    @Test
+    public void eatMeat() {
+        try {
+            assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
+        } catch (Exception e){
+            assertNull(e);
+        }
+
+    }
+
+    @Test
+    public void getFamily() {
+        assertEquals(feline.getFamily(), "Кошачьи");
+    }
+
+    @Test
+    public void getKittens() {
+        assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    public void testGetKittens() {
+        assertEquals(100, feline.getKittens(100));
+    }
+}
