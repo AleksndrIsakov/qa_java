e849eabf4e05f33f6612fe31bc02b5eafa3ff73f
Aleksandr Isakov <isakov.a.n@yandex.ru>
2022-02-19 23:32:49 +0300

    Добавлены Unit тесты для класса Animal

---
+++ src/test/java/AnimalTest.java
@@ -0,0 +1,54 @@
+import com.example.Animal;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class AnimalTest {
+
+    private Animal animal;
+    private final String animalType;
+
+    public AnimalTest(String animalType) {
+        this.animalType = animalType;
+        this.animal = new Animal();
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"Хищник}"},
+                {"Травоядное"},
+                {"Непонятное"}
+        };
+    }
+
+    @Test
+    public void getFood() {
+        List<String> expected = null;
+        switch (animalType) {
+            case "Хищник":
+                expected = List.of("Животные", "Птицы", "Рыба");
+                break;
+            case "Травоядное":
+                expected = List.of("Трава", "Различные растения");
+                break;
+        }
+
+        try {
+            assertEquals(expected, animal.getFood(animalType));
+        } catch (Exception e) {
+            assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", e.getMessage());
+        }
+    }
+
+    @Test
+    public void getFamily() {
+        assertEquals("Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи", animal.getFamily());
+    }
+}
